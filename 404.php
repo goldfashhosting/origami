fbcfd229a40cfdff2b19a30208b58fc1b7371ca5
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-11-02 19:04:08 -0500

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="post">
+	<center /><a href="http://www.rfig.us/.GO/url?GET.ref=Po.St_Cli&owa_medium=NOTFOUND&owa_source=www.maxxpayout.com&owa_campaign=NFP&owa_ad=Winners+Maxxpayout+GLB&owa_ad_type=NOTFOUND?los=http://www.maxxpayout.com/?owa_medium=NOTFOUND&owa_source=www.maxxpayout.com&owa_campaign=NFP&owa_ad=Winners+Maxxpayout+GLB&owa_ad_type=NOTFOUND" onclick="OWATracker.shareStateByLink(this.href); return false;">We Can't Find What Your Looking For! Click here to go home.</a></center>
+</div>
+
+<?php get_footer(); ?>
